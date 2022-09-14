5f59f32e42025ba82e1725af5598c4214b25badc
Ubuntu <Adediwura@Hometoaliens.45enuvbboozuha2bvm0snxfvce.bx.internal.cloudapp.net>
2022-09-14 07:46:54 +0000

    new modfications to app.py

--- app.py
+++ app.py
@@ -31,6 +31,4 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
     app.run(port=5151, debug=True)
