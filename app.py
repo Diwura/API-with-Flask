d77972114a4e0e71d1075e5212486f516f47e367
Ubuntu <Adediwura@Hometoaliens.45enuvbboozuha2bvm0snxfvce.bx.internal.cloudapp.net>
2022-09-14 10:10:19 +0000

    modification

--- app.py
+++ app.py
@@ -30,9 +30,15 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 
-from db import db
-db.init_app(app)
-
 if __name__ == '__main__':
-    
-    app.run(port=5151, debug=True)
+      from db import db
+      db.init_app(app)
+
+      if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+            
+            
+        app.run(port=5151, debug=True)
+
